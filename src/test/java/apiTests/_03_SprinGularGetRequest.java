283d9915a8096ed55dac139f28ff7178a9488017
Mete <hasanozyer06@gmail.com>
2022-09-23 21:59:31 +0300

    Added Day_8 _04_TestWithParametre.class

--- src/test/java/apiTests/_03_SprinGularGetRequest.java
+++ src/test/java/apiTests/_03_SprinGularGetRequest.java
@@ -11,7 +11,7 @@ import static io.restassured.RestAssured.*;
 public class _03_SprinGularGetRequest {
 
     String accessToken = "Bearer " +
-            "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjM3OTE5MTAsInN1YiI6Ik1yaW5tb3lNYWp1bWRhciIsInVzZXJJZCI6Imd1aWRlcnNvZnQiLCJyb2xlIjoiVVNFUiJ9.O50vgt3hUP4yV_3QpRmybgmhte5rONdDmNG_pmOLWT0";
+            "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjM5NTU2MTgsInN1YiI6Ik1yaW5tb3lNYWp1bWRhciIsInVzZXJJZCI6Imd1aWRlcnNvZnQiLCJyb2xlIjoiVVNFUiJ9.PJ7JZf5vpsRM13ZMQBdKPc3EEQaXWFTAITakloRR2Ms";
     String springularUrl = "http://142.93.110.12:9119";
 
     @Test
@@ -19,7 +19,28 @@ public class _03_SprinGularGetRequest {
         Response response = given().header("Authorization", accessToken)
                 .when().get(springularUrl + "/api/orders");
 
+        response.prettyPrint();
+
         Assert.assertEquals(response.statusCode(),200);
+
+    }
+
+    @Test
+    public void test02(){
+        Response response = given().header("Authorization", accessToken)
+                .when().get(springularUrl + "/api/orders?orderid=4003");
+        response.prettyPrint();
+
+    }
+
+    @Test
+    public void test03(){
+        Response response = given().header("Authorization", accessToken)
+                .when().get(springularUrl + "/api/orders?orderid=4500");
+        response.prettyPrint();
+
+        Assert.assertEquals(response.statusCode(),200);
+
 
     }
 
