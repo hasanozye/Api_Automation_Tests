b4906e25b8b88329398ea0afa75a92c45f38a61f
Mete <hasanozyer06@gmail.com>
2022-09-28 22:53:20 +0300

    _04_TestWithParametre.java is updated

--- src/test/java/apiTests/_04_TestWithParametre.java
+++ src/test/java/apiTests/_04_TestWithParametre.java
@@ -26,7 +26,7 @@ public class _04_TestWithParametre {
 
         assertEquals(response.statusCode(), 200);
         assertEquals(response.contentType(), "application/json");
-        assertTrue(response.body().asString().contains("doggi"));
+        assertTrue(response.body().asString().contains("doggie"));
 
 
     }
